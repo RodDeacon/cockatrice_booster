d8c87fbfaf58dce4fda53a92e43a49afd7dbdec2
RodDeacon <rdeacon@mail.greenriver.edu>
2021-05-17 13:36:56 -0700

    updated code with create_deck function

--- cockatrice_booster.cpp
+++ cockatrice_booster.cpp
@@ -13,17 +13,23 @@
 const std::string commons   = "stx-commons.dec";
 const std::string uncommons = "stx-uncommons.dec";
 const std::string rares     = "stx-rare-mythics.dec";
+const std::string deck      = "output.dec";
 
 // amount of cards
 const int cNum = 10;    // commons
 const int uNum = 3;     // uncommons
 const int rNum = 1;     // rares
 
+// amount of boosters
+const int BOOSTER_AMOUNT = 1;
+
 
 // function prototypes
 void create_map(std::map<std::string, std::string> &);
 void create_map(std::map<std::string , std::string> &, std::string, std::string);
 void print_map(std::map<std::string, std::string>);
+// void create_deck(std::map<std::string, std::string>, std::map<std::string, int> &);
+
 
 
 int main()
@@ -32,8 +38,12 @@ int main()
     std::map<std::string, std::string> cardList; // make a map of commons, uncommons, rares
 
     create_map(cardList);
+    print_map(cardList);
 
-    
+    std::map<std::string, int> deck;  
+
+   // create_deck(cardList, deck);
+
 
     system("pause");
     return 0;
@@ -55,9 +65,11 @@ void create_map(std::map<std::string, std::string> &map)
 
 void create_map(std::map<std::string, std::string> &map, std::string fName, std::string rarity)
 {
+
     std::cout << "running second create map" << std::endl;
 
     std::ifstream file(fName);
+
 
     int x;              // numbers of cards in deck
     std::string y;      // name of the card
@@ -65,24 +77,35 @@ void create_map(std::map<std::string, std::string> &map, std::string fName, std:
 
     while (std::getline(file, y))
     {
-    
-    
+
         std::cout << "adding " << x << " cards named " << y << "\n" << "rarity: " << rarity << "\n" << std::endl; // debug
         map.emplace(rarity, y);
     
     }
     
-    // for each element, print out the map 
-
-
-
-
 }
+
+
 
 void print_map(std::map<std::string, std::string> map)
 {
+
     for (auto itr = map.begin(); itr != map.end(); itr++)
     {
-        std::cout << *itr;
+        std::cout << itr->first << itr->second << std::endl;
     }
+
 }
+
+
+// Create a deck file consisting of the correct amount of cards
+// void create_deck(std::map<std::string, std::string> card_set, std::map<std::string, int> &outputDeck)
+// {
+
+//     std::ofstream out_file(deck); // output file
+
+    
+
+//     out_file.close();
+
+// }
